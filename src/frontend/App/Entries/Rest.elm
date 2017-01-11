a6ab3707320c113df2cd5569cbe0bc05a3dcadd2
Jack Zhang <15jzhang@gmail.com>
2017-01-11 03:32:31 -0500

    Implement Entries.Rest Json Decoders and Encoders

---
+++ src/frontend/App/Entries/Rest.elm
@@ -0,0 +1,55 @@
+module App.Entries.Rest exposing (..)
+
+import Http
+import Task
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+import App.Entries.Types as Entries exposing (..)
+
+
+-- RESOURCES
+
+entriesUrl : String
+entriesUrl =
+    "http://localhost:3000/api/entries"
+
+
+entryUrl : Int -> String
+entryUrl id =
+    String.join "/" [ entriesUrl, (toString id) ]
+
+
+-- DECODERS
+
+entriesDecoder : Decode.Decoder (List Entry)
+entriesDecoder =
+    Decode.list entryDecoder
+
+
+entryDecoder : Decode.Decoder Entry
+entryDecoder =
+    Decode.map3 Entry
+        (field "id" Decode.int)
+        (field "text" Decode.string)
+        (field "complete" Decode.bool)
+
+
+-- ENCODERS
+
+entryEncoder : Int -> String -> Bool -> Encode.Value
+entryEncoder id text complete =
+    let fields =
+        [ ( "id", id )
+        , ( "text", text )
+        , ( "complete", complete )
+        ]
+    in
+        fields
+            |> Encode.object
+
+
+-- COMMANDS
+
+fetchAll : Cmd Entries.Msg
+
